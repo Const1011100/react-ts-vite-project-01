260c6cf3855044a0962e20fed1f8dc10b136bac4
Const1011100 <const1011100@gmail.com>
2025-09-17 19:32:38 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+export function App() {
+  const tracks = [
+    {
+      id: 1,
+      title: 'Musicfun soundtrack',
+      url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3',
+    },
+    {
+      id: 2,
+      title: 'Musicfun soundtrack instrumental',
+      url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3',
+    },
+  ];
+
+  const newLiReactElements = tracks.map((track) => {
+    return (
+      <li key={track.id}>
+        <div>{track.title}</div>
+        <audio src={track.url} controls></audio>
+      </li>
+    );
+  });
+
+  return (
+    <>
+      <h1>Musicfun Player</h1>
+      <ul>{newLiReactElements}</ul>
+    </>
+  );
+}
