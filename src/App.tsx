0fd59f44c992ee1cc81e3c32d2a7a9e89434684c
Const1011100 <const1011100@gmail.com>
2025-09-20 21:32:25 +0300

    lesson 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,72 @@
+import { useState } from 'react';
+
+//const tracks = null;
+const tracks = [
+  {
+    id: 1,
+    title: 'Musicfun soundtrack',
+    url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3',
+    isSelected: false,
+  },
+  {
+    id: 2,
+    title: 'Musicfun soundtrack instrumental',
+    url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3',
+    isSelected: true,
+  },
+];
+
 export function App() {
-  const tracks = [
-    {
-      id: 1,
-      title: 'Musicfun soundtrack',
-      url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3',
-    },
-    {
-      id: 2,
-      title: 'Musicfun soundtrack instrumental',
-      url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3',
-    },
-  ];
+  const [selectedTrackId, setSelectedTrackId] = useState(null);
+
+  if (tracks === null) {
+    return (
+      <>
+        <h1>Musicfun Player</h1>
+        <span>Loading...</span>
+      </>
+    );
+  }
 
   const newLiReactElements = tracks.map((track) => {
     return (
-      <li key={track.id}>
-        <div>{track.title}</div>
+      <li
+        key={track.id}
+        style={{
+          border: track.id === selectedTrackId ? '1px solid orange' : 'none',
+        }}
+      >
+        <div
+          onClick={() => {
+            setSelectedTrackId(track.id);
+          }}
+        >
+          {track.title}
+        </div>
         <audio src={track.url} controls></audio>
       </li>
     );
   });
 
+  if (tracks.length === 0) {
+    return (
+      <>
+        <h1>Musicfun Player</h1>
+        <span>No tracks</span>
+      </>
+    );
+  }
+
   return (
     <>
       <h1>Musicfun Player</h1>
+      <button
+        onClick={() => {
+          setSelectedTrackId(null);
+        }}
+      >
+        Reset selection
+      </button>
       <ul>{newLiReactElements}</ul>
     </>
   );
